3fb284fdccb6eb0575fc6e030343220f1a211cd4
Robson Vinicius <robsondgn@outlook.com>
2019-03-15 17:09:33 -0300

    $_GET[nome] $_GET[preco]

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -2,7 +2,8 @@
 
 <?php
   $nome = $_GET["nome"];  
+  $preco = $_GET["preco"];
 ?>
- <p>Produto <?php echo $nome; ?> adicionado com sucesso!</p>
+ <p>Produto <?php echo $nome; ?>, <?php echo $preco ?> adicionado com sucesso!</p>
 
 </html>
