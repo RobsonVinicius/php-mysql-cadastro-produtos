034f165e582b521cf3e035c1db6e5cfc65004db7
Robson Vinicius <robsondgn@outlook.com>
2019-03-25 14:48:48 -0300

    criando arquivo de cabecalho e rodape

---
+++ cabecalho.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Minha Loja</title>
+  <link href="css/boostrap.css" rel="stylesheet" />
+  <link href="css/loja.css" rel="stylesheet" />
+</head>
+
+<body>
+
+  <div class="container">
+    <div class="principal">
+    <!-- fim cabecalho.php -->
+    
---
+++ rodape.php
@@ -0,0 +1,7 @@
+<!-- início do rodapé -->
+    
+    </div>  <!-- fim da div principal --> 
+  </div>  <!-- fim da div container --> 
+  
+</body>
+</html>
