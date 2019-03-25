fdcbfad01a900454f86a45764ebcabae3f86a5af
Robson Vinicius <robsondgn@outlook.com>
2019-03-25 15:59:05 -0300

    corrigindo link bootstrap

--- include/cabecalho.php
+++ include/cabecalho.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Minha Loja</title>
-  <link href="css/boostrap.css" rel="stylesheet" />
+  <link href="css/bootstrap.css" rel="stylesheet" />
   <link href="css/loja.css" rel="stylesheet" />
 </head>
 
