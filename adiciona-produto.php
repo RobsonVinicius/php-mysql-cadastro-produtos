416dbaab4d0c0280f18fe82e5a0497e6c6353261
Robson Vinicius <robsondgn@outlook.com>
2019-04-05 15:01:02 -0300

    criando funcao insere produto

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,26 +1,30 @@
 <?php include("include/cabecalho.php") ?>
 
-<?php
-  $nome = $_GET["nome"];  
-  $preco = $_GET["preco"];
+  <?php
+    $nome = $_GET["nome"];  
+    $preco = $_GET["preco"];
 
-  $conexao = mysqli_connect('us-cdbr-iron-east-03.cleardb.net', 'bc71f90f15cc38', 'd8157d1c5f54a36 ', 'heroku_e5f79f34483698a');
+    $conexao = mysqli_connect('us-cdbr-iron-east-03.cleardb.net', 'bc71f90f15cc38', 'd8157d1c5f54a36 ', 'heroku_e5f79f34483698a');
 
-  $query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+    function insereProduto($conexao, $nome, $preco) {
+      $query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+      $resultadoDaInsercao = mysqli_query($conexao, $query);
+      return $resultadoDaInsercao;
+    }
 
-  if(mysqli_query($conexao, $query)) {
-?>
+    if(insereProduto($conexao, $nome, $preco)) {
+  ?>
 
-<p class="alert-sucess">Produto <?=$nome;?>, <?=$preco;?> foi adicionado com sucesso!</p>
+  <p class="alert-sucess">Produto <?=$nome;?>, <?=$preco;?> foi adicionado com sucesso!</p>
 
-<?php 
-  } else {
-?>
+  <?php 
+    } else {
+  ?>
 
-<p class="alert-danger"> O produto <?=$nome;?>, não foi adicionado</p>
+  <p class="alert-danger"> O produto <?=$nome;?>, não foi adicionado</p>
 
-<?php
-  }  
-?>
+  <?php
+    }  
+  ?>
 
 <?php include("include/rodape.php") ?>
