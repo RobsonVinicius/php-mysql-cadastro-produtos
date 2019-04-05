75637f0479fa1c69cfb79304e63612c9507a5b8a
Robson Vinicius <robsondgn@outlook.com>
2019-04-05 18:03:57 -0300

    Criando menu

--- include/cabecalho.php
+++ include/cabecalho.php
@@ -11,6 +11,26 @@
 
 <body>
 
+  <div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 navbar-header">
+          <a href="index.php" class="navbar-brand">Minha Loja</a>
+        </div>
+        <div class="col-md-4">
+          <ul class="nav navbar-nav">
+            <li>
+              <a href="produto-formulario.php">Adiciona Produto</a>
+            </li>
+            <li>
+              <a href="sobre.php">Sobre</a>
+            </li>
+          </ul>
+        </div>  
+      </div>
+    </div>
+  </div>
+
   <div class="container">
     <div class="principal">
     <!-- fim cabecalho.php -->
