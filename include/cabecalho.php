44700a78be2cee432f3b4dbf9644240803fba3bd
Robson Vinicius <robsondgn@outlook.com>
2019-05-02 09:51:18 -0300

    teste link

--- include/cabecalho.php
+++ include/cabecalho.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Minha Loja</title>
-  <link href="https://www.https://php-mysql-parte1.herokuapp.com/css/bootstrap.css" rel="stylesheet" />
+  <link href="https://php-mysql-parte1.herokuapp.com/css/bootstrap.css" rel="stylesheet" />
   <link href="https://php-mysql-parte1.herokuapp.com/css/loja.css" rel="stylesheet" />
 </head>
 
