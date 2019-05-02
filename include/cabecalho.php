6bfe057b85ae596e35b60d7bc7a1cff4628a797a
Robson Vinicius <robsondgn@outlook.com>
2019-05-02 10:22:13 -0300

    teste nav

--- include/cabecalho.php
+++ include/cabecalho.php
@@ -5,33 +5,31 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Minha Loja</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-  <link href="https://php-mysql-parte1.herokuapp.com/css/loja.css" rel="stylesheet" />
-</head>
 
+  <link href="https://php-mysql-parte1.herokuapp.com/css/bootstrap.css" rel="stylesheet">
+  <link href="https://php-mysql-parte1.herokuapp.com/css/loja.css" rel="stylesheet">  
+</head>
 <body>
 
   <div class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
-      <div class="row">
+      <div class="row">        
         <div class="col-md-8 navbar-header">
           <a href="index.php" class="navbar-brand">Minha Loja</a>
-        </div>
-        <div class="col-md-4">
+        </div>            
+        <div class="col-md-4"> 
           <ul class="nav navbar-nav">
-            <li>
-              <a href="produto-formulario.php">Adiciona Produto</a>
-            </li>
-            <li>
-              <a href="sobre.php">Sobre</a>
-            </li>
+            <li><a href="produto-formulario.php">Adiciona Produto</a></li>
+            <li><a href="sobre.php">Sobre</a></li>
           </ul>
-        </div>  
+        </div>
       </div>
     </div>
   </div>
 
+
   <div class="container">
     <div class="principal">
-    <!-- fim cabecalho.php -->
+    <!-- fim do Cabeçalho -->
+
     
