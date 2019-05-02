a45cbba8768b4ed8fbf5cf065937adf22459eaa5
Robson Vinicius <robsondgn@outlook.com>
2019-05-02 11:39:10 -0300

    dando forma a tabela

--- produto-formulario.php
+++ produto-formulario.php
@@ -2,10 +2,20 @@
 
   <h1>Formulário de Cadastro</h1>
   <form action="adiciona-produto.php">
-    Nome <input type="text" name="nome" /> <br><br>
-    Preço <input type="number" name="preco" /> <br><br>
-
-    <input type="submit" value="Cadastrar" />
+    <table class="table">
+      <tr>
+        <td>Nome: </td>
+        <td><input type="text" name="nome" /></td>
+      </tr>
+      <tr>
+        <td>Preço</td>
+        <td><input type="number" name="preco" /></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td><input type="submit" value="Cadastrar" /></td>
+      </tr>
+    </table>
   </form>
 
 <?php include("include/rodape.php") ?>
