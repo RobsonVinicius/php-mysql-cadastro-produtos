48e6467b206f6ae22b6811bcd14ecfe77293e076
Robson Vinicius <robsondgn@outlook.com>
2019-05-02 10:08:53 -0300

    teste form

--- produto-formulario.php
+++ produto-formulario.php
@@ -1,8 +1,9 @@
 <?php include("include/cabecalho.php") ?>
 
+  <h1>Formulário de Cadastro</h1>
   <form action="adiciona-produto.php">
-    Nome <input type="text" name="nome" /> <br>
-    Preço <input type="number" name="preco" /> <br>
+    Nome <input type="text" name="nome" /> <br><br>
+    Preço <input type="number" name="preco" /> <br><br>
 
     <input type="submit" value="Cadastrar" />
   </form>
