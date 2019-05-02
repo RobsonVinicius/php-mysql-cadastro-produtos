5bbb31d3f39cb0282b7a76171212a14c18fe5668
Robson Vinicius <robsondgn@outlook.com>
2019-05-02 09:55:06 -0300

    teste link online

--- include/cabecalho.php
+++ include/cabecalho.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Minha Loja</title>
-  <link href="https://php-mysql-parte1.herokuapp.com/css/bootstrap.css" rel="stylesheet" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
   <link href="https://php-mysql-parte1.herokuapp.com/css/loja.css" rel="stylesheet" />
 </head>
 
