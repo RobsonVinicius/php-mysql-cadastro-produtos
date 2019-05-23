49036a322c95fe03a18bdd762cd96534a5dbaea4
Robson Vinicius <robsondgn@outlook.com>
2019-05-23 14:38:00 -0300

    correção da ordem da lista

--- produto-formulario.php
+++ produto-formulario.php
@@ -12,13 +12,14 @@
         <td><input type="number" name="preco" /></td>
       </tr>
       <tr>
-        <td></td>
-        <td><input type="submit" value="Cadastrar" /></td>
-      </tr>
-      <tr>
         <td>Descrição</td>
         <td><textarea name="descricao" class="form-control"></textarea></td>
       </tr>
+      <tr>
+        <td></td>
+        <td><input type="submit" value="Cadastrar" /></td>
+      </tr>
+      
     </table>
   </form>
 
