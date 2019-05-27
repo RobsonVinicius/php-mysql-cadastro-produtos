a0d05d05ceacbc125e6902b6f0707a807faf59ea
Robson Vinicius <robsondgn@outlook.com>
2019-05-27 15:40:53 -0300

    correção categoria

--- include/banco-produto.php
+++ include/banco-produto.php
@@ -1,5 +1,5 @@
 <?php 
-  function insereProduto($conexao, $nome, $preco, $descricao) {     
+  function insereProduto($conexao, $nome, $preco, $descricao, $categoria_id) {     
     $query = "insert into produtos (nome, preco, descricao, categoria_id) values ('{$nome}', {$preco},'{$descricao}', {$categoria_id}"; 
     $resultadoDaInsercao = mysqli_query($conexao, $query);     
     return $resultadoDaInsercao;
