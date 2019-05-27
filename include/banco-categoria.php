0cef50f70200c3a2da85f5622fe064f70ba928c3
Robson Vinicius <robsondgn@outlook.com>
2019-05-27 15:11:44 -0300

    banco categoria - relacionando produtos com categoria

---
+++ include/banco-categoria.php
@@ -0,0 +1,10 @@
+<?php 
+  function listaCategorias($conexao) {
+    $categorias = array();
+    $query = "select * from categorias";
+    $resultado = mysqli_query($conexao, $query);
+    while($categoria = mysqli_fetch_assoc($resultado)) {
+      array_push($categorias, $categoria);
+    }
+    return $categorias;
+  }
--- produto-formulario.php
+++ produto-formulario.php
@@ -1,19 +1,36 @@
-<?php include("include/cabecalho.php") ?>
+<?php 
+  include("include/cabecalho.php");
+  include("include/conexao.php");
+  include("include/banco-categoria.php");
+
+  $categorias = listaCategorias($conexao);
+?>
 
   <h1>Formulário de Cadastro</h1>
   <form action="adiciona-produto.php" method="post">
     <table class="table">
       <tr>
         <td>Nome:</td>
-        <td><input type="text" name="nome"/></td>
+        <td><input class="form-control" type="text" name="nome"/></td>
       </tr>
       <tr>
         <td>Preço</td>
-        <td><input type="number" name="preco"/></td>
+        <td><input class="form-control" type="number" name="preco"/></td>
       </tr>
       <tr>
         <td>Descrição</td>
         <td><textarea class="form-control" name="descricao"></textarea></td>
+      </tr>
+      <tr>
+        <td>
+          <?php
+            foreach($categorias as $categoria) :
+          ?>
+          <input type="radio" name="categoria_id" value="<?= $categoria['id'] ?>"><?=$categoria['nome'] ?><br>
+          <?php 
+            endforeach
+          ?>
+        </td>
       </tr>
       <tr>
         <td></td>
