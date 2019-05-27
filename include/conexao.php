51a61eded5f4003b2f553782890f8cd9bb5bb606
Robson Vinicius <robsondgn@outlook.com>
2019-05-27 11:50:59 -0300

    teste bd remotemysql

--- include/conexao.php
+++ include/conexao.php
@@ -1,2 +1,2 @@
 <?php
-  $conexao = mysqli_connect('localhost', 'id6873553_robsonwordpress', '153153153', 'id6873553_mybase');
+  $conexao = mysqli_connect('remotemysql.com:3306', 'RQqe9xuTMF', 'tpd47zDFde', 'RQqe9xuTMF');
