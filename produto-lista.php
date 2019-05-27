452393c19b545116a9bfb624811b04686f45e5aa
Robson Vinicius <robsondgn@outlook.com>
2019-05-27 18:55:57 -0300

    correção produto-lista.php

--- produto-lista.php
+++ produto-lista.php
@@ -19,7 +19,7 @@
     <td><?= $produto['nome'] ?></td>
     <td><?= $produto['preco'] ?></td>
     <td><?= substr($produto['descricao'], 0, 15) ?></td>
-    <td><?= $produto['$categoria_nome'] ?></td>
+    <td><?= $produto['categoria_nome'] ?></td>
     <td>
       <form action="remove-produto.php" method="post">
         <input type="hidden" name="id" value="<?= $produto['id'] ?>" />
