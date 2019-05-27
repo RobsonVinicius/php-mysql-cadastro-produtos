1fc24930b99ca3ad05684168dfb45ccf0a4f1fae
Robson Vinicius <robsondgn@outlook.com>
2019-05-27 18:17:34 -0300

    correção function listaProdutos

--- include/banco-produto.php
+++ include/banco-produto.php
@@ -7,10 +7,10 @@
 
   function listaProdutos($conexao) {
     $produtos = array();
-    $resultado = mysqli_query($conexao, "select p.*,c.nome as categoria_nome from produtos as p join categorias as c on p.categoria_id = c.id");
+    $resultado = mysqli_query($conexao, "select p.*, c.nome as categoria_nome from produtos as p join categorias as c on p.categoria_id = c.id");
 
     while($produto = mysqli_fetch_assoc($resultado)) {
-    array_push($produtos, $produto);
+      array_push($produtos, $produto);
     }
     return $produtos;
   }
