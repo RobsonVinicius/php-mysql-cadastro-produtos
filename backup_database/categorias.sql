114d9081f22e7bcdfc73aed72f8ff607b32aa641
Robson Vinicius <robsondgn@outlook.com>
2019-06-24 11:42:40 -0300

    backup tabelas categorias e produtos

---
+++ backup_database/categorias.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: 24-Jun-2019 às 14:41
+-- Versão do servidor: 8.0.13-4
+-- versão do PHP: 7.2.19-0ubuntu0.18.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `RQqe9xuTMF`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `nome`) VALUES
+(1, 'esporte'),
+(2, 'escolar'),
+(3, 'mobilidade'),
+(5, 'Doces');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- backup_database/produtos.sql
+++ backup_database/produtos.sql
@@ -2,10 +2,10 @@
 -- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: 25-Abr-2019 às 21:59
--- Versão do servidor: 8.0.15
--- versão do PHP: 7.2.17-0ubuntu0.18.04.1
+-- Host: 127.0.0.1
+-- Generation Time: 24-Jun-2019 às 16:32
+-- Versão do servidor: 10.1.38-MariaDB
+-- versão do PHP: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `mikrail`
+-- Database: `loja`
 --
 
 -- --------------------------------------------------------
@@ -31,34 +31,31 @@ SET time_zone = "+00:00";
 CREATE TABLE `produtos` (
   `id` int(11) NOT NULL,
   `nome` varchar(255) DEFAULT NULL,
-  `preco` decimal(10,2) DEFAULT NULL
+  `preco` decimal(10,2) DEFAULT NULL,
+  `descricao` text,
+  `categoria_id` int(11) DEFAULT NULL,
+  `usado` tinyint(1) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `produtos`
 --
 
-INSERT INTO `produtos` (`id`, `nome`, `preco`) VALUES
-(1, 'Carro', '20000.00'),
-(2, 'motocicleta', '10000.00'),
-(3, 'bicicleta', '500.00'),
-(4, 'lÃ¡pis', '2.00'),
-(5, 'lÃ¡pis', '0.00'),
-(6, 'caneta', '0.00'),
-(7, 'camiseta', '0.00'),
-(8, 'camiseta', '0.00'),
-(9, 'cabeca', '0.00'),
-(10, 'cabeca', '0.00'),
-(11, 'xicara', '0.00'),
-(12, 'aviÃ£o', '1000000.00'),
-(13, 'celular', '1200.00'),
-(14, 'copo', '2.00'),
-(15, 'lapis ', '33.00'),
-(16, 'calca', '2.00'),
-(17, 'casaco', '40.00'),
-(18, 'canetas', '2.00'),
-(19, 'cafe', '5.00'),
-(20, 'carreta', '5.00');
+INSERT INTO `produtos` (`id`, `nome`, `preco`, `descricao`, `categoria_id`, `usado`) VALUES
+(2, 'motocicleta ', '10000.00', 'Harley Davidson', 3, 1),
+(3, 'bicicleta', '500.00', 'Descricao deste produto', 3, 0),
+(4, 'lÃ¡pis', '3.00', 'Descricao deste produto', 3, 0),
+(6, 'caneta', '0.00', 'Descricao deste produto', 3, 0),
+(7, 'camiseta', '0.00', 'Descricao deste produto', 3, 0),
+(11, 'xicara', '0.00', 'Descricao deste produto', 3, 0),
+(12, 'aviÃ£o', '1000000.00', 'Descricao deste produto', 3, 0),
+(13, 'celular', '1200.00', 'Descricao deste produto', 3, 0),
+(15, 'Caneta ', '99.00', 'Caneta bic padrÃ£o, ou seja, cumpre a sua funÃ§Ã£o.', 3, 0),
+(20, 'Bola de Futebol', '20000.00', 'Bola de futebol com controle remoto, para burlar os jogos e fazer o seu time vencer.', 1, 0),
+(21, 'Caneta de prata', '900.00', 'Caneta de prata', 2, 0),
+(22, 'Caneca', '9.00', 'Caneca de plÃ¡stico', 2, 0),
+(23, 'Caneca de vidro', '29.00', 'Caneca de vidro ', 2, 1),
+(24, 'Suporte', '99.00', 'Suporte automotivo para celular', 3, 0);
 
 --
 -- Indexes for dumped tables
@@ -78,7 +75,7 @@ ALTER TABLE `produtos`
 -- AUTO_INCREMENT for table `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
