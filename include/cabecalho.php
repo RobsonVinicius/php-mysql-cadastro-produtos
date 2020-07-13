7a92e867d595c0d782eab8c1049eadc5d60911f9
Robson Vinicius <robsondgn@outlook.com>
2020-07-13 14:10:56 -0300

    Correção link css - por troca de nome do repo

--- include/cabecalho.php
+++ include/cabecalho.php
@@ -6,8 +6,8 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Minha Loja</title>
 
-  <link href="https://php-mysql-parte1.herokuapp.com/css/bootstrap.css" rel="stylesheet">
-  <link href="https://php-mysql-parte1.herokuapp.com/css/loja.css" rel="stylesheet">  
+  <link href="../css/bootstrap.css" rel="stylesheet">
+  <link href="../css/loja.css" rel="stylesheet">  
 </head>
 <body>
 
